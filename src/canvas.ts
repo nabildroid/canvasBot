32ca862eadb7f2c81edebe6b3ccba7b652d18388
nabildroid <pni20156789@gmail.com>
2021-02-21 19:58:01 +0100

    code refactoring

--- src/canvas.ts
+++ src/canvas.ts
@@ -4,14 +4,17 @@ import { AnnonceType, Announcement } from "./models/types";
 import Turndown from "turndown";
 export default class Canvas implements ICanvas {
 	server: AxiosInstance;
-	courses: { [x: string]: string };
+	courses: { [courseName: string]: string };
 
 	cache: {
 		announcements: number[];
 	};
+	html2md: Turndown;
 
 	constructor(token: string) {
 		this.server = this.initServer(token);
+		this.html2md = new Turndown();
+
 		this.courses = {};
 
 		this.cache = {
@@ -31,44 +34,54 @@ export default class Canvas implements ICanvas {
 	}
 
 	async getCourseAnnouncements(courseName: string) {
-		const turndownService = new Turndown();
+		const id = this.courses[courseName];
+		const announcements = await this.requestAnnounces([id]);
 
+		return this.parseAnnouncements(announcements);
+	}
+
+	async getAnnouncements() {
+		const ids = Object.values(this.courses);
+		const announcements = await this.requestAnnounces(ids);
+
+		return this.parseAnnouncements(announcements);
+	}
+
+	requestAnnounces(courseIds: string[]) {
+		const context_codes = courseIds.map((v) => `course_${v}`);
 		return this.server
 			.get<any[]>(`announcements`, {
 				params: {
-					"context_codes[]": `course_${this.courses[courseName]}`,
+					context_codes,
 				},
 			})
-			.then((response) => {
-				return response.data
-					.map<Announcement>((d) => ({
-						id: d.id,
-						author: d.author.display_name,
-						date: new Date(d.posted_at),
-						message: turndownService.turndown(d.message),
-						title: d.title,
-						url: d.url,
-						module: courseName,
-						type: this.getAnnouncementType(d.title, d.message),
-					}))
-					.filter(({ id }) => !this.cache.announcements.includes(id))
-					.map((announce) => {
-						this.cache.announcements.push(announce.id);
-						return announce;
-					});
-			});
+			.then((r) => r.data);
 	}
 
+	findCourseNameFromContextCode(code: string) {
+		for (const course in this.courses) {
+			if (`course_${this.courses[course]}` == code) return course;
+		}
+		throw Error("couse code doesn't match the courses in Canvas Object");
+	}
+
+	parseAnnouncements(announcements: any[]) {
+		return announcements.map<Announcement>((d) => ({
+			id: d.id,
+			author: d.author.display_name,
+			date: new Date(d.posted_at),
+			message: this.html2md.turndown(d.message),
+			title: d.title,
+			url: d.url,
+			module: this.findCourseNameFromContextCode(d.context_code),
+			type: this.getAnnouncementType(d.title, d.message),
+		}));
+	}
 	getAnnouncementType(title: string, content: string) {
 		if (title.includes("zoom") || content.includes("zoom"))
 			return AnnonceType.ZOOM;
 
 		return AnnonceType.NONE;
-	}
-
-	getAnnouncements() {
-		//TODO get announcements from all avaliable courses
-		return Promise.resolve([]);
 	}
 
 	async fetchCourses() {
--- src/models/icanvas.ts
+++ src/models/icanvas.ts
@@ -2,9 +2,6 @@ import { AxiosInstance } from "axios";
 import { Announcement } from "./types";
 
 export default interface ICanvas {
-	server: AxiosInstance;
-	courses: { [key in string]: string };
-	initServer: (token: string) => AxiosInstance;
 	addCourse: (name: string, id: string) => void;
 	getCourseAnnouncements: (name: string) => Promise<Announcement[]>;
 	getAnnouncements:()=>Promise<Announcement[]>;
